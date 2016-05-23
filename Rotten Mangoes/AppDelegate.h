55b5619b8dcc804594e4355eda3d615deabad026
ascoelho <anthonystefancoelho@gmail.com>
2016-05-23 11:12:30 -0400

    first commit

---
+++ Rotten Mangoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rotten Mangoes
+//
+//  Created by Anthony Coelho on 2016-05-23.
+//  Copyright © 2016 Anthony Coelho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Rotten Mangoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rotten Mangoes
+//
+//  Created by Anthony Coelho on 2016-05-23.
+//  Copyright © 2016 Anthony Coelho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
